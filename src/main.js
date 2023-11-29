fb7039f17d0289bb11158016499323e467e3866c
ArandaCampos <renan.aranda.campos.rac@gmail.com>
2023-11-29 15:48:48 -0300

    version 2.0 (add visibility and days)

--- src/main.js
+++ src/main.js
@@ -1,20 +1,3 @@
-/*
- *  Rastro:
- *      const points = [];
-        points.push( new THREE.Vector3( - 10, 0, 0 ) );
-        points.push( new THREE.Vector3( 0, 10, 0 ) );
-        points.push( new THREE.Vector3( 10, 0, 0 ) );
-
-        const geometry = new THREE.BufferGeometry().setFromPoints( points );
-        const line = new THREE.Line( geometry, material );
-        scene.add( line );
-
-        renderer.render( scene, camera );
- *
- *
- *      scene.children[10].geometry.attributes.position.array
- */
-
 var camera;
 var scene;
 var renderer;
